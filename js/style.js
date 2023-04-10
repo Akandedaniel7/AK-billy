220d3b1f505382dcfea0d5ad70c914ba848e8798
Ajilore Daniel <94573586+Dnaux-dev@users.noreply.github.com>
2023-04-10 09:16:14 -0700

    ll

---
+++ js/style.js
@@ -0,0 +1,114 @@
+let slider = tns({
+  container : ".my-slider",
+  "slideBy" : 1,
+  "speed" : 400,
+  "nav" : false,
+  controlsContainer : '#controls',
+  prevButton : ".previous",
+  nextButton : ".next",
+  responsive: {
+    640: {
+      items: 2,
+      gutter: 10,
+    },
+    1000: {
+      items: 3,
+      gutter: 10,
+    },
+    1400: {
+      items: 4,
+      gutter: 10
+    },
+
+    375: {
+      items: 1
+    }
+  }
+})
+
+
+let slider1 = tns({
+  container : ".my-slider1",
+  "slideBy" : 1,
+  "speed" : 400,
+  "nav" : false,
+  controlsContainer : '#controls1',
+  prevButton : ".previous1",
+  nextButton : ".next1",
+  responsive: {
+    640: {
+      items: 2,
+      gutter: 10,
+    },
+    1000: {
+      items: 3,
+      gutter: 10,
+    },
+    1400: {
+      items: 5,
+      gutter: 10
+    },
+
+    375: {
+      items: 1
+    }
+  }
+})
+
+
+let slider2 = tns({
+  container : ".my-slider2",
+  "slideBy" : 1,
+  "speed" : 400,
+  "nav" : false,
+  controlsContainer : '#controls2',
+  prevButton : ".previous2",
+  nextButton : ".next2",
+  responsive: {
+    640: {
+      items: 2,
+      gutter: 10,
+    },
+    1000: {
+      items: 3,
+      gutter: 10,
+    },
+    1400: {
+      items: 5,
+      gutter: 10
+    },
+
+    414: {
+      items: 1,
+    }
+  }
+})
+
+
+let slider3 = tns({
+  container : ".my-slider3",
+  "slideBy" : 1,
+  "speed" : 400,
+  "nav" : false,
+  controlsContainer : '#controls3',
+  prevButton : ".previous3",
+  nextButton : ".next3",
+  responsive: {
+    640: {
+      items: 2,
+      gutter: 10,
+    },
+    1000: {
+      items: 3,
+      gutter: 10,
+    },
+    1400: {
+      items: 5,
+      gutter: 10
+    },
+
+    414: {
+      items: 1
+    }
+  }
+})
